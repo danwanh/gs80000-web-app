cfadbfdc14c9bb141bbeddb7e2a9f7627b366e71
danwanh <dananh890@gmail.com>
2025-10-07 09:18:45 +0700

    fix: change gs80000 page content

--- app/product/gs80000/page.tsx
+++ app/product/gs80000/page.tsx
@@ -10,45 +10,16 @@ export default function gs80000Page() {
               <h2 className="text-white font-small text-lg">DIGITAL-GS-80000詳細</h2>
               <div className="text-sm text-gray-700 leading-relaxed space-y-3"></div>
             </div>
-            {/* <Image src="/product_ttmax_018.jpg" 
+            <Image src="/gs80000_005.jpg" 
                             alt="image" 
-                            width={900}
+                            width={500}
                             height={256}
-                            className="rounded mb-4"
-                          /> */}
-
-            {/* Sub header bar */}
-            <div className="bg-gray-100 border-l-4 border-l-[#0071bc] border-b-4 border-b-gray-300 p-2 mt-10">
-                <h3 className="text-[#0071bc] font-bold text-lg">電子ペンとローラーで部分ケア</h3>
+                            className="ml-50 rounded mb-4"
+                          />
+            <div className="bg-gray-100 border-l-4 border-l-[#0071bc] border-b-4 border-b-gray-300 p-2 mb-10">
+                <h3 className="text-[#0071bc] font-bold text-lg">豊富な治療モードを搭載</h3>
                 <div className="text-sm text-gray-700 leading-relaxed space-y-3"></div>
             </div>
-
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 grid grid-cols-2 lg:grid-cols-2 mb-10">
-                  <div>
-                    <Image src="/product_ttmax_026_01.jpg" 
-                            alt="image" 
-                            width={300}
-                            height={300}
-                            className="rounded mb-4"
-                          />
-                  </div>
-                  <div className="space-y-5">
-                      <p>
-                          肩や首のこりなどは、痛みを感じる所(ツボ)などを局所的に押したりもんだりする
-                          ことで効果を得る事が出来ます。
-                      </p>
-                      <p>
-                        針･灸･指圧などと同じ治療法です。
-                      </p>
-                      <p>
-                        そこで本器では、電子ペンやローラーを装備しました。
-                      </p>
-                      <p>
-                        熟練した技術などがなくても、自分ひとりで肩や首などのケアが出来ます。
-                      </p> 
-                  </div>
-            </div>
-
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 grid grid-cols-2 lg:grid-cols-2 gap-8">
                 <div>
                 <Image src="/product_ttmax_019.jpg" 
@@ -74,6 +45,38 @@ export default function gs80000Page() {
                         className="rounded mb-4"
                         />
                 </div>
+            </div>
+
+                        {/* Sub header bar */}
+            <div className="bg-gray-100 border-l-4 border-l-[#0071bc] border-b-4 border-b-gray-300 p-2 mt-10">
+                <h3 className="text-[#0071bc] font-bold text-lg">電子ペンとローラーで部分ケア</h3>
+                <div className="text-sm text-gray-700 leading-relaxed space-y-3"></div>
+            </div>
+
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 grid grid-cols-2 lg:grid-cols-2 mb-10">
+                  <div>
+                    <Image src="/gs80000_004.jpg" 
+                            alt="image" 
+                            width={300}
+                            height={300}
+                            className="rounded mb-4"
+                          />
+                  </div>
+                  <div className="space-y-5">
+                      <p>
+                          肩や首のこりなどは、痛みを感じる所(ツボ)などを局所的に押したりもんだりする
+                          ことで効果を得る事が出来ます。
+                      </p>
+                      <p>
+                        針･灸･指圧などと同じ治療法です。
+                      </p>
+                      <p>
+                        そこで本器では、電子ペンやローラーを装備しました。
+                      </p>
+                      <p>
+                        熟練した技術などがなくても、自分ひとりで肩や首などのケアが出来ます。
+                      </p> 
+                  </div>
             </div>
         </div>
       </div>
